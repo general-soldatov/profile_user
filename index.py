d1908969d2c8f7288a061b05f7b1dcd1bdb54300
general-soldatov <general-soldatov@mail.ru>
2024-09-18 16:59:38 +0300

    Add style and image files, setting fast-api server

--- index.py
+++ index.py
@@ -1,28 +1,35 @@
 import awsgi
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 templates = Jinja2Templates(directory='app/templates')
+app.mount('/static', StaticFiles(directory='app/static'), 'static')
 
 student = {
-    "phone_number": "string",
-    "first_name": "string",
-    "last_name": "string",
-    "date_of_birth": "2024-07-23",
-    "email": "user@example.com",
-    "address": "stringstri",
-    "enrollment_year": 2002,
+    "name": "string",
+    "range": "string",
+    "img": "https://img.razrisyika.ru/kart/125/499535-smeshariki-losyash-35.jpg",
+    "rate": 45,
+    "star": 3,
+    "info": {
+        'profile': 'HTTC',
+        'variant': 3,
+        'group': 34
+    },
     "major_id": 1,
     "major": "stringstri",
     "course": 1,
     "special_notes": "string"
 }
 
+variant = 'Permission denied'
+
 @app.get("/")
 async def root(request: Request):
     return templates.TemplateResponse(name='students.html',
-                                      context={'request': request, 'student': student})
+                                      context={'request': request, 'student': student, 'variant': variant})
 
 def handler(event, context):
     return awsgi.response(app, event, context)
